6a563fa82a0f8ace5b6bfd019a94bbeaaee54565
Callum Irving <callum.irving@protonmail.com>
2021-10-31 12:17:05 -0400

    fix: make package functional

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # Social Network Visualiser (snvis)
 
 This is a tool to visualise social networks from a spreadsheet of connections.
+
+## Installing
+
+```sh
+pip install snvis
+```
+
+Python 3.9 or greater is required.
+
+## Usage
+
+Once you have installed `snvis`, you can run it using `snvis` as long as you
+have added the directory it was installed to to your path. If this doesn't work,
+you can run:
+
+```sh
+python -m snvis
+```
+
+The only required argument is the spreadsheet to parse, in tab-separated values
+format.
+
+An example usage would be:
+
+```sh
+snvis data.tsv -v
+```
+
+This runs on the file `data.tsv` in verbose mode so that you can see what the
+program is doing.
+
+To see all options, run:
+
+```sh
+snvis -h
+```
+
+## Contributing
+
+### Commits
+
+When writing commit messages, please use conventional commits.
+
+### Building
+
+To build a release, install build using 
+
+```sh
+pip install build
+```
+
+then to build the package, run
+
+```sh
+python -m build
+```
--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md") as readme:
     setup(
         name="snvis",
-        version="0.1.0",
+        version="0.1.4",
         description="Social network visualising tool",
         long_description=readme.read(),
+        long_description_content_type="text/markdown",
         url="https://github.com/Callum-Irving/snvis",
         author="Callum Irving",
         author_email="callum.irving04@gmail.com",
         license="MIT",
-        packages=["snvis"],
-        install_requires=("thefuzz >= 0.19.0", "python-igraph >= 0.9.8"),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.9",
+        ],
+        package_dir={"": "."},
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=["thefuzz >= 0.19.0", "python-igraph >= 0.9.8",
+                          "python-Levenshtein>=0.12.2", "cairocffi>=1.3.0"],
         entry_points={"console_scripts": ["snvis = snvis.__main__:main"]}
     )
